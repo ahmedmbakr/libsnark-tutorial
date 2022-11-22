4df5b4843e1b329c840306f1c345b25384df06f2
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2022-11-22 10:11:09 +0200

    - Modifications to integrate cryptopp lib - Test integration inside test-gadget.cpp    * Test includes AES encryption

--- src/test-gadget.cpp
+++ src/test-gadget.cpp
@@ -8,9 +8,94 @@
 
 #include "gadget.hpp"
 #include "util.hpp"
+#include <cryptopp/rijndael.h>
+#include <cryptopp/aes.h>
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/files.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/hex.h"
 
 using namespace libsnark;
 using namespace std;
+using namespace CryptoPP;
+
+void encrypt_using_AES_test()
+{
+    AutoSeededRandomPool prng;
+    HexEncoder encoder(new FileSink(std::cout));
+
+    SecByteBlock key(AES::DEFAULT_KEYLENGTH);
+    SecByteBlock iv(AES::BLOCKSIZE);
+
+    prng.GenerateBlock(key, key.size());
+    prng.GenerateBlock(iv, iv.size());
+
+    std::string plain = "CBC Mode Test";
+    std::string cipher, recovered;
+
+    std::cout << "plain text: " << plain << std::endl;
+
+    /*********************************\
+    \*********************************/
+
+    try
+    {
+        CBC_Mode< AES >::Encryption e;
+        e.SetKeyWithIV(key, key.size(), iv);
+
+        StringSource s(plain, true,
+                       new StreamTransformationFilter(e,
+                                                      new StringSink(cipher)
+                       ) // StreamTransformationFilter
+        ); // StringSource
+    }
+    catch(const Exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        exit(1);
+    }
+
+    /*********************************\
+    \*********************************/
+
+    std::cout << "key: ";
+    encoder.Put(key, key.size());
+    encoder.MessageEnd();
+    std::cout << std::endl;
+
+    std::cout << "iv: ";
+    encoder.Put(iv, iv.size());
+    encoder.MessageEnd();
+    std::cout << std::endl;
+
+    std::cout << "cipher text: ";
+    encoder.Put((const byte*)&cipher[0], cipher.size());
+    encoder.MessageEnd();
+    std::cout << std::endl;
+
+    /*********************************\
+    \*********************************/
+
+    try
+    {
+        CBC_Mode< AES >::Decryption d;
+        d.SetKeyWithIV(key, key.size(), iv);
+
+        StringSource s(cipher, true,
+                       new StreamTransformationFilter(d,
+                                                      new StringSink(recovered)
+                       ) // StreamTransformationFilter
+        ); // StringSource
+
+        std::cout << "recovered text: " << recovered << std::endl;
+    }
+    catch(const Exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        exit(1);
+    }
+}
 
 int main()
 {
@@ -66,6 +151,12 @@ int main()
 
   print_vk_to_file<default_r1cs_ppzksnark_pp>(vk, "../build/vk_data");
   print_proof_to_file<default_r1cs_ppzksnark_pp>(proof, "../build/proof_data");
+
+    cout << "key length: " << AES::DEFAULT_KEYLENGTH << endl;
+    cout << "key length (min): " << AES::MIN_KEYLENGTH << endl;
+    cout << "key length (max): " << AES::MAX_KEYLENGTH << endl;
+    cout << "block size: " << AES::BLOCKSIZE << endl;
+    encrypt_using_AES_test();
 
   return 0;
 }
